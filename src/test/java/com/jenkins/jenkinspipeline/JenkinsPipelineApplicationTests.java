8052fb38e5ed6013e10ecf4dc847568a71c1dcdd
Mohamed Qotrany <mohamedkorany600@gmail.com>
2020-10-11 13:42:40 +0200

    First commit

---
+++ src/test/java/com/jenkins/jenkinspipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
